475a281f490612002ef284f599b7749be5156848
YenChunYu <jenny98417rib@gmail.com>
2023-11-13 15:38:30 +0800

    1. Tabbar ViewController 2. Create tabbar items 3. Create SearchViewController, SearchCell

---
+++ TravelTogether/TravelTogether/SearchPage/SearchMemoriesTableVIewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SearchMemories.swift
+//  TravelTogether
+//
+//  Created by User on 2023/11/13.
+//
+
+import UIKit
+
+class SearchMemoriesCell: UITableViewCell {
+    
+    
+    
+    
+    
+}
---
+++ TravelTogether/TravelTogether/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTogether
+//
+//  Created by User on 2023/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
